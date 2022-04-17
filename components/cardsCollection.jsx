b6b2132cc71211599d0363b27c41d33c7574eb1d
nguo <nguo@users.noreply.github.com>
2022-04-17 12:53:41 -0700

    change masonry breakpoints for fewer cards

--- components/cardsCollection.jsx
+++ components/cardsCollection.jsx
@@ -1,14 +1,15 @@
 import Masonry from 'react-masonry-css'
 
-const breakpointColumnsObj = {
-  default: 5,
-  1441: 4,
-  1200: 3,
-  990: 2,
-  700: 1
-}
-
 export default function CardsCollection({ children }) {
+  let maxCols = Math.min(children.length, 5)
+  const breakpointColumnsObj = {
+    default: maxCols,
+    1441: Math.min(maxCols, 4),
+    1200: Math.min(maxCols, 3),
+    990: Math.min(maxCols, 2),
+    700: 1
+  }
+
   return (
     <Masonry breakpointCols={breakpointColumnsObj} className="masonry-grid" columnClassName="masonry-column">
       {children}
